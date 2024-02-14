6b2306a2ce5c53e3fd5c7c4feca4ded3792d7b2a
Tejas Sodhi <sodhi.tejas@gmail.com>
2024-02-14 00:16:49 -0500

    client interactive for TCPClient

--- client/TCPClient.java
+++ client/TCPClient.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 
 
 public class TCPClient {
+    
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java client.Client <serverIP> <port>");
@@ -19,37 +20,69 @@ public class TCPClient {
         String serverIP = args[0];
         int serverPort = Integer.parseInt(args[1]);
 
-        Socket socket = null;
-        try {
-            socket = new Socket(serverIP, serverPort);
+        try (Socket socket = new Socket(serverIP, serverPort)) {
             socket.setSoTimeout(5000);
+
+            System.out.println("Connected to the server");
+            ClientLogger.log("Connected to the server");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         try (
-                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
         ) {
-            System.out.println("Connected to the server");
-            ClientLogger.log("Connected to the server");
+            // prepopulate the data
+            populateKeyValues(in, out);
 
-            int n = 100;
-            for(int i = 0; i < n; i++) {
-                UUID uuid = UUID.randomUUID();
-                String requestId = uuid.toString();
+            // this code snippet is used to make the client interactive
+            while (true) {
+                System.out.println("Which operation do you want to use?");
+                System.out.println("1. PUT");
+                System.out.println("2. GET");
+                System.out.println("3. DELETE");
+                System.out.print("Enter your choice (1/2/3): ");
+                String choice = userInput.readLine();
 
-                String putString = requestId + "::PUT::key" + i + "::value" + i;
-                sendRequest(out, in, putString);
-            }
+                String operation;
+                switch (choice) {
+                    case "1":
+                        operation = "PUT";
+                        break;
+                    case "2":
+                        operation = "GET";
+                        break;
+                    case "3":
+                        operation = "DELETE";
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+                        continue;
+                }
 
-            for(int i = 0; i < n; i++) {
-                UUID uuid = UUID.randomUUID();
-                String requestId = uuid.toString();
+                if (operation.equals("PUT")) {
+                    System.out.print("Please enter the key (integer): ");
+                    String key = userInput.readLine();
+                    System.out.print("Please enter the value for the key (integer): ");
+                    String value = userInput.readLine();
+                    String requestId = UUID.randomUUID().toString();
+                    String request = requestId + "::" + operation + "::" + key + "::value" + value;
+                } else {
+                    System.out.print("Please enter the key (integer): ");
+                    String key = userInput.readLine();
+                    String requestId = UUID.randomUUID().toString();
+                    String request = requestId + "::" + operation + "::" + key;
+                }
 
-                String getString = requestId + "::GET::key" + i;
-                sendRequest(out, in, getString);
+                sendRequest(out, in, request);
+
+                System.out.print("Do you want to perform another operation? (yes/no): ");
+                String anotherOperation = userInput.readLine().toLowerCase();
+                if (!anotherOperation.equals("yes")) {
+                    break;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,6 +104,38 @@ public class TCPClient {
             String requestId = strArr[0];
             System.out.println("Received no response from the server for request id : "+requestId);
             ClientLogger.log("Received no response from the server for the request id : "+requestId);
+        }
+    }
+
+    private static void populateKeyValues(BufferedReader in, PrintWriter out) {
+
+        NUM_KEYS = 10
+        try {
+            // PUT requests
+            for (int i = 1; i <= NUM_KEYS; i++) {
+                UUID uuid = UUID.randomUUID();
+                String requestId = uuid.toString();
+                String key = Integer.toString(i);
+                String value = Integer.toString(i * 10);
+                String putString = requestId + "::PUT::key" + key + "::value" + value;
+
+                sendRequest(out, in, putString);
+                System.out.println("Prepopulated key " + key + " with value " + value);
+                ClientLogger.log("Prepopulated key " + key + " with value " + value);
+            }
+            //GET requests
+            for (int i = 1; i <= NUM_KEYS; i++) {
+                UUID uuid = UUID.randomUUID();
+                String requestId = uuid.toString();
+                String key = Integer.toString(i);
+                String getString = requestId + "::GET::key" + key;
+
+                sendRequest(out, in, getString);
+                System.out.println("Prepopulated key " + key + " with value " + value);
+                ClientLogger.log("Prepopulated key " + key + " with value " + value);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
